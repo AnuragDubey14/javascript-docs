622268a1231d46100d2535ce3774d7ab8ccb9520
AnuragTekuncorked <anurag.intern@tekuncorked.com>
2025-06-20 12:41:41 +0530

    Basic setup

---
+++ 02_Code-Structure/script.js
@@ -0,0 +1,42 @@
+alert('Hello'); alert('World'); //will work cause statement separate by semicolon;
+
+alert('Hello');
+alert('World'); 
+ 
+// both are separate lines 
+
+alert('Hello')
+alert('World')
+
+// both are separated by new line characters 
+
+alert(3 +
+1
++ 2);
+
+// display 6 cause it's an expression 
+
+
+alert("Hello");
+
+[1, 2].forEach(alert);
+
+//separated by semi colon
+
+alert("Hello")
+
+[1, 2].forEach(alert);
+
+// not work not separated by semi colon
+
+// single line comments 
+
+
+/*
+multiline comments 
+*/
+
+
+
+
+
